021b3c9f59f5afdbd7a929dcd62fc613011b3f63
cari050429 <102688451+cari050429@users.noreply.github.com>
2023-07-17 15:47:16 +0000

    "url"

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://127.0.0.1:8000/api/';
+const baseURL = 'https://wildlifedtn-backend.azurewebsites.net/api/';
 
 const axiosInstance = axios.create({
 	baseURL: baseURL,
