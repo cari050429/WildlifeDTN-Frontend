a669f982cc5e92ec1d27c8114fe2c9c0bcfa1122
Yeranis Corrales Carmenate <ycorrales2016@fau.edu>
2023-07-17 08:37:02 -0400

    build

--- src/Searchlisthumidity.js
+++ src/Searchlisthumidity.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Searchlist({ results }) {
+export default function Searchlisthumidity({ results }) {
   return (
     <div className='search-list'>
       {results.map(result => (
         <div className='data-preview' key={result.pk}>
-            <Link to={`/Search/temperature/${result.pk}`}>
-            <p>Data Temperature: {result.temperature}</p>
+            <Link to={`/Search/humidity/${result.pk}`}>
+            <p>Data Temperature: {result.humidity}</p>
           </Link>
         </div>
       ))}
     </div>
   );
 }
-
