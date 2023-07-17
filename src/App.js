e2cef41f492887772f446bdd379720d01e5ca8bc
cari050429 <102688451+cari050429@users.noreply.github.com>
2023-07-17 15:10:05 +0000

    "changed homepage"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Login from './Login';
 import Search from './Search';
 import Searchdetail from './Searchdetail';
@@ -16,14 +16,14 @@ const App = () => {
 
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Login onLoginSuccess={handleLoginSuccess} />} />
-        <Route exact path="/Search" element={isAuthenticated ? <Search /> : <Navigate to="/" />} />
-        <Route exact path="/Search/temperature/:pk" element={isAuthenticated ? <Searchdetail /> : <Navigate to="/" />} />
-        <Route exact path="/Search/humidity/:pk" element={isAuthenticated ? <Searchdetailhumidity /> : <Navigate to="/" />} />
-        <Route exact path="/Search/picture/:pk" element={isAuthenticated ? <Searchdetailpicture /> : <Navigate to="/" />} />
-        <Route exact path="/Logout" element={isAuthenticated ? <Logout /> : <Navigate to="/" />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Login onLoginSuccess={handleLoginSuccess} />} />
+          <Route exact path="/Search" element={isAuthenticated ? <Search /> : <Navigate to="/" />} />
+          <Route exact path="/Search/temperature/:pk" element={isAuthenticated ? <Searchdetail /> : <Navigate to="/" />} />
+          <Route exact path="/Search/humidity/:pk" element={isAuthenticated ? <Searchdetailhumidity /> : <Navigate to="/" />} />
+          <Route exact path="/Search/picture/:pk" element={isAuthenticated ? <Searchdetailpicture /> : <Navigate to="/" />} />
+          <Route exact path="/Logout" element={isAuthenticated ? <Logout /> : <Navigate to="/" />} />
+        </Routes>
     </div>
   );
 };
